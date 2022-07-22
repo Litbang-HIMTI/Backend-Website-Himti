910c22a4f2f9ded671df3b43c7926b50605ca3e6
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-07-22 13:08:03 +0700

    refactor: add null data and status code to auth error
    - return user data on check
    - add req.session check
    - also change 200 to 401 on check

--- src/controllers/v1/auth.ts
+++ src/controllers/v1/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
 import { userModel } from "../../models/user";
 
 export const validatePassword = async (username: string, password: string) => {
@@ -7,53 +8,53 @@ export const validatePassword = async (username: string, password: string) => {
 };
 
 export const validateLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.user) {
+	if (req.session && req.session.user) {
 		next();
 	} else {
-		res.status(401).json({ message: "Need to be logged in" });
+		res.status(401).json({ data: null, message: "Need to be logged in", success: false });
 	}
 };
 
 export const validateAdmin = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.user && req.session.role?.includes("admin")) {
+	if (req.session && req.session.user && req.session.role?.includes("admin")) {
 		next();
 	} else {
-		res.status(403).json({ message: "Need to be admin" });
+		res.status(403).json({ data: null, message: "Need to be admin", success: false });
 	}
 };
 
 export const validateEditor = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.user && (req.session.role?.includes("editor") || req.session.role?.includes("admin"))) {
+	if (req.session && req.session.user && (req.session.role?.includes("editor") || req.session.role?.includes("admin"))) {
 		next();
 	} else {
-		res.status(403).json({ message: "Need to be editor" });
+		res.status(403).json({ data: null, message: "Need to be editor", success: false });
 	}
 };
 
 export const validateForumMod = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.user && (req.session.role?.includes("forum_moderator") || req.session.role?.includes("admin"))) {
+	if (req.session && req.session.user && (req.session.role?.includes("forum_moderator") || req.session.role?.includes("admin"))) {
 		next();
 	} else {
-		res.status(403).json({ message: "Need to be forum moderator" });
+		res.status(403).json({ data: null, message: "Need to be forum moderator", success: false });
 	}
 };
 
 export const validateShortlinkMod = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.user && (req.session.role?.includes("shortlink_moderator") || req.session.role?.includes("admin"))) {
+	if (req.session && req.session.user && (req.session.role?.includes("shortlink_moderator") || req.session.role?.includes("admin"))) {
 		next();
 	} else {
-		res.status(403).json({ message: "Need to be shortlink moderator" });
+		res.status(403).json({ data: null, message: "Need to be shortlink moderator", success: false });
 	}
 };
 
 export const login = async (req: Request, res: Response) => {
 	const { username, password } = req.body;
-	if (!username) return res.status(400).json({ message: "Username is required", success: false });
-	if (!password) return res.status(400).json({ message: "Password is required", success: false });
+	if (!username) return res.status(400).json({ data: null, message: "Username is required", success: false });
+	if (!password) return res.status(400).json({ data: null, message: "Password is required", success: false });
 
 	const { user, valid } = await validatePassword(username, password);
-	if (!user) return res.status(401).json({ message: "Invalid username or password", success: false });
-	if (!valid) return res.status(401).json({ message: "Invalid username or password", success: false });
+	if (!user) return res.status(401).json({ data: null, message: "Invalid username or password", success: false });
+	if (!valid) return res.status(401).json({ data: null, message: "Invalid username or password", success: false });
 
 	// save session
 	req.session.userId = user._id;
@@ -61,30 +62,38 @@ export const login = async (req: Request, res: Response) => {
 	req.session.role = user.role;
 
 	return res.status(200).json({
+		data: null,
 		message: "Login successful",
 		success: true,
 	});
 };
 
 export const logout = async (req: Request, res: Response) => {
-	req.session.destroy((err) => {
-		if (err) return res.status(500).json({ message: `Error logging out. ${err}`, success: false });
+	if (req.session)
+		return req.session.destroy((err) => {
+			if (err) return res.status(500).json({ data: null, message: `Error logging out. ${err}`, success: false });
 
-		return res.status(200).json({
-			message: "Logout successful",
-			success: true,
+			return res.status(200).json({
+				data: null,
+				message: "Logout successful",
+				success: true,
+			});
 		});
-	});
+	else return res.status(401).json({ data: null, message: "Not logged in", success: false });
 };
 
 export const check = async (req: Request, res: Response) => {
-	if (req.session.user) {
+	if (req.session && req.session.user) {
+		// get user data
+		const user = await userModel.findById(Types.ObjectId(req.session.userId)).select("-salt -hash");
 		return res.status(200).json({
+			data: user,
 			message: "Logged in",
 			success: true,
 		});
 	} else {
-		return res.status(200).json({
+		return res.status(401).json({
+			data: null,
 			message: "Not logged in",
 			success: false,
 		});
